55b5aca0604aedc448fe5088cafb996bd1f67182
HarlemSquirrel <wittyman37@yahoo.com>
2016-01-17 15:29:27 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,16 @@
+require 'pry'
+
 def nyc_pigeon_organizer(data)
   # write your code here!
+  pidgeon_list = {}
+  data.each do |attribute, attribute_data|
+    attribute_data.each do |type, names_array|
+      names_array.each do |name|
+        pidgeon_list[name] = {} if pidgeon_list[name] == nil
+        pidgeon_list[name][attribute] = [] if pidgeon_list[name][attribute] == nil
+        pidgeon_list[name][attribute] << type.to_s
+      end
+    end
+  end
+  pidgeon_list
 end
